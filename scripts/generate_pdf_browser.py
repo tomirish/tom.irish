953475f13d5598333cfb8a9a14b2c8f022170de1
Tom Irish <tom@tom.irish>
2026-02-08 20:42:58 -0800

    update PDF script

--- scripts/generate_pdf_browser.py
+++ scripts/generate_pdf_browser.py
@@ -80,7 +80,7 @@ def main():
                     'bottom': '0.2in',
                     'left': '0.2in'
                 },
-                scale=0.99  # Slightly smaller to fit more content on one page
+                scale=0.98  # Slightly smaller to fit more content on one page
             )
             
             browser.close()
